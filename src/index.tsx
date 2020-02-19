5fb2be6f6dde5617bf0252ec769a820d8c4c1725
WistreHosshii <sao_heath6147.wistre@icloud.com>
2020-02-20 01:29:59 +0900

    material-uiの適用順序をstuled-components優先に

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { StylesProvider } from "@material-ui/styles";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <StylesProvider injectFirst>
+      <App />
+    </StylesProvider>
   </Router>,
   document.getElementById("root")
 );
