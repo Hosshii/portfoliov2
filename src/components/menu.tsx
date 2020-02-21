3d9ad7ff69039dfb4e4ac827a8e3e452c2260d41
WistreHosshii <sao_heath6147.wistre@icloud.com>
2020-02-22 03:52:26 +0900

    多分フォーマットが変わっただけ

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,15 +1,15 @@
-import React, { Fragment } from "react";
-import styled from "styled-components";
-import { Transition } from "react-transition-group";
-import { TransitionStatus } from "react-transition-group/Transition";
-import { Button } from "@material-ui/core";
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+import { Transition } from 'react-transition-group'
+import { TransitionStatus } from 'react-transition-group/Transition'
+import { Button } from '@material-ui/core'
 // import * as H from "history";
-import history from "../common/history";
-import Frame from "../common/frame";
+import history from '../common/history'
+import Frame from '../common/frame'
 
 interface State {
-  isMenuOpen: boolean;
-  isFirst: boolean;
+  isMenuOpen: boolean
+  isFirst: boolean
 }
 // interface Props {
 //   history: React.CSSProperties;
@@ -17,15 +17,15 @@ interface State {
 
 export default class Menu extends React.Component<{}, State> {
   constructor(props: any) {
-    super(props);
-    this.toggleOpenMenu = this.toggleOpenMenu.bind(this);
-    this.state = { isMenuOpen: false, isFirst: true };
+    super(props)
+    this.toggleOpenMenu = this.toggleOpenMenu.bind(this)
+    this.state = { isMenuOpen: false, isFirst: true }
   }
   toggleOpenMenu() {
     this.setState((state: State) => ({
       isMenuOpen: !state.isMenuOpen,
       isFirst: false
-    }));
+    }))
   }
 
   render() {
@@ -37,28 +37,18 @@ export default class Menu extends React.Component<{}, State> {
         <MenuWrapper>
           <Transition in={this.state.isMenuOpen} timeout={1000}>
             {state => (
-              <Frame
-                isMenuOpen={this.state.isMenuOpen}
-                isFirst={this.state.isFirst}
-                time={0.8}
-              >
-                {state === "entered" && (
+              <Frame isMenuOpen={this.state.isMenuOpen} isFirst={this.state.isFirst} time={0.8}>
+                {state === 'entered' && (
                   <Fragment>
                     <MenuTitle state={state}>menu</MenuTitle>
                     <MenuItem state={state} delay={0.2}>
-                      <ItemButton onClick={() => history.push("/")}>
-                        top
-                      </ItemButton>
+                      <ItemButton onClick={() => history.push('/')}>top</ItemButton>
                     </MenuItem>
                     <MenuItem state={state} delay={0.4}>
-                      <ItemButton onClick={() => history.push("/about")}>
-                        about
-                      </ItemButton>
+                      <ItemButton onClick={() => history.push('/about')}>about</ItemButton>
                     </MenuItem>
                     <MenuItem state={state} delay={0.6}>
-                      <ItemButton onClick={() => history.push("/work")}>
-                        work
-                      </ItemButton>
+                      <ItemButton onClick={() => history.push('/work')}>work</ItemButton>
                     </MenuItem>
                   </Fragment>
                 )}
@@ -67,12 +57,12 @@ export default class Menu extends React.Component<{}, State> {
           </Transition>
         </MenuWrapper>
       </Fragment>
-    );
+    )
   }
 }
 
 const getMenuTitleStyle = (props: TransitionStatus) => {
-  if (props === "entered") {
+  if (props === 'entered') {
     return `
       animation: show-menu-title 0.5s ease forwards ;
       @keyframes show-menu-title {
@@ -85,11 +75,11 @@ const getMenuTitleStyle = (props: TransitionStatus) => {
           transform: translateX(10px);
         }
       }
-    `;
+    `
   }
-};
+}
 const getMenuItemStyle = (props: TransitionStatus, delay: number) => {
-  if (props === "entered") {
+  if (props === 'entered') {
     return `
       animation: show-menu-text 0.5s ease ${delay}s forwards ;
       @keyframes show-menu-text {
@@ -102,15 +92,15 @@ const getMenuItemStyle = (props: TransitionStatus, delay: number) => {
           transform: translateX(-5px);
         }
       }
-    `;
+    `
   }
-};
+}
 const ToggleButtonWrapper = styled.div`
   position: fixed;
   /* position: absolute; */
   right: 10px;
   z-index: 10;
-`;
+`
 const ToggleButton = styled(Button)`
   background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
   border-radius: 3px;
@@ -119,26 +109,26 @@ const ToggleButton = styled(Button)`
   height: 48px;
   padding: 0 30px;
   box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
-`;
+`
 
 const ItemButton = styled(Button)`
   color: #76fb58;
   background-color: #090b0a;
-`;
+`
 
 const MenuTitle = styled.div<{ state: TransitionStatus }>`
   ${({ state }) => getMenuTitleStyle(state)}
   width: 100%;
   text-align: left;
   color: #76fb58;
-`;
+`
 
 const MenuItem = styled.div<{ state: TransitionStatus; delay: number }>`
   ${({ state, delay }) => getMenuItemStyle(state, delay)}
   width: 100%;
   text-align: right;
   opacity: 0;
-`;
+`
 
 const MenuWrapper = styled.div`
   box-sizing: border-box;
@@ -149,4 +139,4 @@ const MenuWrapper = styled.div`
   top: 50px;
   position: fixed;
   z-index: 10;
-`;
+`
