a2c458475047d21446070d5c407cae58f680144e
WistreHosshii <sao_heath6147.wistre@icloud.com>
2020-02-24 02:42:31 +0900

    add myCard

---
+++ src/common/myCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface MyCardProps {
+  width: React.CSSProperties['width']
+  height: React.CSSProperties['height']
+  textAlign: React.CSSProperties['textAlign']
+}
+const MyCard = styled.div<Partial<MyCardProps>>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  text-align: ${({ textAlign }) => textAlign};
+  position: relative;
+  padding: 10px 0;
+  background-color: red;
+`
+
+MyCard.defaultProps = {
+  width: undefined,
+  height: undefined,
+  textAlign: 'center'
+}
+export default MyCard
