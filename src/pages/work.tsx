732cb8337d762f1637f72fb4964665ea0e1fb34a
WistreHosshii <sao_heath6147.wistre@icloud.com>
2020-02-25 03:44:01 +0900

    workページを追加

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,19 +1,50 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
+import Title from '../components/title'
+import { device } from '../utils/const'
+import WorkCard, { Content } from '../components/aboutcard'
 
 export default class Work extends React.Component {
   constructor(props: any) {
-    super(props);
+    super(props)
   }
   render() {
     return (
       <Wrapper>
-        <div>work</div>
+        <Title title="Work" />
+        <WorkWrapper>
+          <WorkCard title={toolTitle} content={toolContent} bottom={toolBottom}></WorkCard>
+        </WorkWrapper>
       </Wrapper>
-    );
+    )
   }
 }
 
 const Wrapper = styled.div`
-  display: grid;
-`;
+  box-sizing: border-box;
+  position: relative;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  @media ${device.mobileS} {
+    left: 0;
+    width: 100%;
+  }
+  @media ${device.laptop} {
+    left: 130px;
+    width: calc(100% - 260px);
+  }
+`
+
+const WorkWrapper = styled.div`
+  width: 100%;
+  max-width: 800px;
+  text-align: center;
+  margin: 0 auto;
+`
+const toolTitle = 'このサイト'
+const toolContent: Content[] = [
+  { title: '使用技術', content: 'React, typescript' },
+  { title: '作成時期', content: 'この春休み' }
+]
+const toolBottom = '自分一人でちゃんと作ったサイトはこれが初めてです'
