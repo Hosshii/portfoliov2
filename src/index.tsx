4d202a1b0c1d2f1f528d873d047bb196631d7817
WistreHosshii <sao_heath6147.wistre@icloud.com>
2020-02-25 03:43:30 +0900

    アイコン追加

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import App from './App'
 import { StylesProvider } from '@material-ui/styles'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faBars, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import { RevealGlobalStyles } from 'react-genie'
 import { MyGlobalstyle } from './utils/style'
 
-library.add(faGithub, faTwitter)
+library.add(faGithub, faTwitter, faBars, faExternalLinkAlt)
 
 ReactDOM.render(
   <StylesProvider injectFirst>
