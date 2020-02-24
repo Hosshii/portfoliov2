08cb3bd8f167ef86b1882ca93f60928dd33e51f9
WistreHosshii <sao_heath6147.wistre@icloud.com>
2020-02-25 03:42:03 +0900

    use jsx in cardContent and use component
    contentにリンクも入れられる様にした。中身の部分を別のコンポーネントにした

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,9 @@
 import React, { Fragment } from 'react'
 import styled, { keyframes } from 'styled-components'
-import MyCard from '../common/myCard'
-import Frame from '../common/frame'
-import CustomFont from '../common/customfont'
-import AboutCard from '../components/aboutcard'
-import { FontSize, device } from '../utils/const'
+import AboutCard, { Content } from '../components/aboutcard'
+import { device, url, FontColor } from '../utils/const'
+import Title from '../components/title'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default class About extends React.Component {
   constructor(props: any) {
@@ -14,57 +13,40 @@ export default class About extends React.Component {
     return (
       <Fragment>
         <Wrapper>
-          <TitleWrapper>
-            <Title>
-              <Frame isMenuOpen={true} fadeInFrame={fadeInFrame} fadeOutFrame={fadeOutFrame}>
-                <CenterWrapper>
-                  <CustomFont delay={1} time={0.5} size={FontSize.title}>
-                    About
-                  </CustomFont>
-                </CenterWrapper>
-              </Frame>
-            </Title>
-          </TitleWrapper>
+          <Title title="about" />
           <AboutWrapper>
-            <AboutCard />
+            <AboutCard title={profileTitle} content={profileContent} />
+            <AboutCard title={languageTitle} content={languageContent} bottom={langBottom} />
+            <AboutCard title={interestTitle} content={interestContent} />
+            <AboutCard title={likeTitle} content={likeContent} bottom={likeBottom} />
+            <AboutCard title={toolTitle} content={toolContent} />
+            <AboutCard title={linkTitle} content={linkContent} />
           </AboutWrapper>
         </Wrapper>
       </Fragment>
     )
   }
 }
-
+const show = keyframes`
+  0% {
+      /* display: none; */
+      opacity: 0;
+    }
+  100% {
+    /* display: block; */
+    opacity: 1;
+  }
+`
 const AboutWrapper = styled.div`
   width: 100%;
   max-width: 800px;
   text-align: center;
   margin: 0 auto;
+  animation: ${show} 1s ease 1.5s forwards;
+  opacity: 0;
+  /* display: none; */
 `
 
-const CenterWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-  background-color: black;
-`
-
-const TitleWrapper = styled.div`
-  height: 200px;
-  text-align: center;
-`
-const Title = styled.div`
-  position: relative;
-  margin: 0 auto;
-  width: 100%;
-  max-width: 500px;
-  height: 60px;
-  top: 10px;
-  text-align: center;
-  box-sizing: border-box;
-`
 const Wrapper = styled.div`
   box-sizing: border-box;
   position: relative;
@@ -80,46 +62,85 @@ const Wrapper = styled.div`
     width: calc(100% - 260px);
   }
 `
-const fadeInFrame = keyframes`
-    0% {
-      width: 0;
-      height: 0;
-      right: 50%;
-      top: 50%;
-      opacity: 0;
-    }
-    50% {
-      width: 80%;
-      height: 1px;
-      right: 10%;
-      top: 50%;
-      opacity: 1;
-    }
-    100% {
-      top: 0;
-      right: 0;
-      width: 100%;
-      height: 100%;
-    }
-`
+const profileTitle = 'Profile'
+const profileContent: Content[] = [
+  {
+    title: 'HN',
+    content: 'hosshii(WistreHosshii)'
+  },
+  {
+    title: '所属',
+    content: '東京工業大学理学院一年'
+  },
+  {
+    title: 'サークル',
+    content: '東京工業大学デジタル創作同好会traP'
+  }
+]
+const languageTitle = 'Programming..etc'
+const languageContent: Content[] = [
+  { content: 'html/css/javascript' },
+  { content: 'React.js/Vue.js' },
+  { content: 'golang,Docker' },
+  { content: 'C++' }
+]
+const langBottom = '下の方はあまり使ったことないです'
 
-const fadeOutFrame = keyframes`
-  0% {
-      width: 100%;
-      height: 100%;
-    }
-  50% {
-    width: 80%;
-    height: 1px;
-    right: 10%;
-    top: 50%;
-    opacity: 1;
-  }
-  100% {
-    width: 0;
-    height: 0;
-    right: 50%;
-    top: 50%;
-    opacity: 0;
+const interestTitle = '興味分野'
+const interestContent: Content[] = [
+  { content: 'web系' },
+  { content: 'css animation' },
+  { content: '低レイヤ系(OS)' },
+  { content: '3D系' }
+]
+
+const likeTitle = '好きなもの'
+const likeContent: Content[] = [
+  { content: 'React.js,Vue.js' },
+  { content: '読書' },
+  { content: 'ホロライブ 、にじさんじ' }
+]
+const likeBottom = 'Vは最近（春休みから）見はじめました'
+
+const toolTitle = 'tools'
+const toolContent: Content[] = [
+  { title: 'エディタ', content: 'VSCode.時々IntelliJ IDEA' },
+  { title: 'OS', content: 'mac OS' }
+]
+
+const StyledA = styled.a`
+  text-decoration: none;
+  :link,
+  :visited,
+  :active {
+    color: ${FontColor.Light};
   }
 `
+const githubLink = () => {
+  return (
+    <Fragment>
+      <StyledA href={url.github} target="_blank">
+        github
+        <FontAwesomeIcon
+          icon={['fas', 'external-link-alt']}
+          style={{ fontSize: 14, marginLeft: '5px', marginBottom: '1px' }}
+        />
+      </StyledA>
+    </Fragment>
+  )
+}
+const twitterLink = () => {
+  return (
+    <Fragment>
+      <StyledA href={url.twitter} target="_blank">
+        twitter
+        <FontAwesomeIcon
+          icon={['fas', 'external-link-alt']}
+          style={{ fontSize: 14, marginLeft: '5px', marginBottom: '1px' }}
+        />
+      </StyledA>
+    </Fragment>
+  )
+}
+const linkTitle = 'Links'
+const linkContent: Content[] = [{ content: githubLink() }, { content: twitterLink() }]
